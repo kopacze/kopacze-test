e7d617315641cd394131e5e9256102b8e3caee5e
kopacze <kopacze_diggers@riseup.net>
2023-06-19 21:04:47 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3eca34dc562b9100cd8f.js"
+    "url": "webpack-runtime-347c9f1745fe864d2fb9.js"
   },
   {
     "url": "framework-de237a875aa45b276a9d.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6f5756a143f921638054487de5eb7b43"
+    "revision": "268590336fd2a5b618d5b9fb93f0f409"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
     "url": "eddf4b2399c53a7d3390a4e0e84fef0b0768cf73-4909ae4f19e7afad80d1.js"
   },
   {
-    "url": "component---src-pages-support-us-js-48819773e86c11d82616.js"
+    "url": "component---src-pages-support-us-js-a86fe15b24a44e077b6c.js"
   },
   {
     "url": "page-data/support-us/page-data.json",
@@ -61,7 +61,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8b31280b22016146af6c4dcf459ba9c7"
+    "revision": "6b97a7b6f87c92249eced1925cb1ebf7"
   },
   {
     "url": "component---src-pages-cooperation-js-ab119d2d9c3702f00986.js"
