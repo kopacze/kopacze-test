1dc212efaaf80d12351c43b5bd4bf895948e9c69
kopacze <kopacze_diggers@riseup.net>
2023-06-19 21:04:27 +0200

    change button style

--- src/components/styledActionButton.js
+++ src/components/styledActionButton.js
@@ -1,14 +1,17 @@
 import styled from "styled-components"
 
 const StyledActionButton = styled.a`
-  border-radius: 20px;
+  border-radius: 12px;
   border: 2px solid black;
   padding: 10px 20px;
-  background: #dc3545;
+  background: #dd4da4;
   color: white;
+  text-decoration: none;
+  font-size: 40px;
+  box-shadow: 2px 2px lightslategrey;
 
   &:hover, &:focus {
-    color: #dc3545;
+    color: #dd4da4;
     background: white;
   }
 `
